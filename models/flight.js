d24ae4605ab1b9413921460c35e410acd3815688
ilysim <illymarie1@live.com>
2022-06-21 20:59:56 -0500

    add ticket to show.ejs

--- models/flight.js
+++ models/flight.js
@@ -2,6 +2,19 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const ticketSchema = new Schema({
+  seat: {
+    type: String,
+    match: /[A-F][1-9]\d?/
+  },
+  price: {
+    type: Number,
+    min: 0
+  }
+}, {
+  timestamps: true
+})
+
 const flightSchema = new Schema({
   airline: {
     type: String,
@@ -27,7 +40,8 @@ const flightSchema = new Schema({
       return today
       // return new Date(new Date().setFullYear(new Date().getFullYear() + 1 ))
     }
-  }, 
+  },
+  tickets: [ticketSchema] 
 }, {
   timestamps: true
 })
